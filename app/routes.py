448273de5a232040c62eb166a1f277d8a4c7777d
3xiced <3xiced@gmail.com>
2021-09-26 10:10:23 +0300

    removed posts

--- app/routes.py
+++ app/routes.py
@@ -12,7 +12,7 @@ def index():
     else:
           user = {'username':username}
           name = username
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', user=user)
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html', title='Sign In')
